0f4aabfdd490ab5676eb4acd4340690f0e8a00f1
Joshua Pepperman <joshua.pepperman@icloud.com>
2019-09-06 13:32:50 -0500

    Swapped alt and meta keys, and got the mouse keys pretty good.

--- keyboards/massdrop/alt/keymaps/jdpepperman/config.h
+++ keyboards/massdrop/alt/keymaps/jdpepperman/config.h
@@ -1,7 +1,50 @@
-#define MOUSEKEY_DELAY                              0
+#define MOUSEKEY_DELAY                              10
 #define MOUSEKEY_INTERVAL                           16
-#define MOUSEKEY_MAX_SPEED                          10
-// #define MOUSEKEY_TIME_TO_MAX                        60
-#define MOUSEKEY_TIME_TO_MAX                        100
-#define MOUSEKEY_WHEEL_MAX_SPEED                    8
-#define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              90
+#define MOUSEKEY_MAX_SPEED                          2
+#define MOUSEKEY_TIME_TO_MAX                        30
+#define MOUSEKEY_CURVE                              300
+#define MOUSEKEY_WHEEL_DELAY                        50
+#define MOUSEKEY_WHEEL_MAX_SPEED                    1
+#define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              0
+
+// This one feels pretty good.
+// #define MOUSEKEY_DELAY                              10
+// #define MOUSEKEY_INTERVAL                           16
+// #define MOUSEKEY_MAX_SPEED                          2
+// #define MOUSEKEY_TIME_TO_MAX                        30
+// #define MOUSEKEY_CURVE                              300
+// #define MOUSEKEY_WHEEL_MAX_SPEED                    8
+// #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              90
+
+// This is close to good, too slow in the beginning.
+// #define MOUSEKEY_DELAY                              100
+// #define MOUSEKEY_INTERVAL                           16
+// #define MOUSEKEY_MAX_SPEED                          2
+// #define MOUSEKEY_TIME_TO_MAX                        100
+// #define MOUSEKEY_WHEEL_MAX_SPEED                    8
+// #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              90
+// #define MOUSEKEY_CURVE                              0
+
+// Still way too fast.
+// #define MOUSEKEY_DELAY                              100
+// #define MOUSEKEY_INTERVAL                           16
+// #define MOUSEKEY_MAX_SPEED                          5
+// #define MOUSEKEY_TIME_TO_MAX                        50
+// #define MOUSEKEY_WHEEL_MAX_SPEED                    8
+// #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              90
+
+// This was too slow at max speed.
+// #define MOUSEKEY_DELAY                              100
+// #define MOUSEKEY_INTERVAL                           16
+// #define MOUSEKEY_MAX_SPEED                          1
+// #define MOUSEKEY_TIME_TO_MAX                        100
+// #define MOUSEKEY_WHEEL_MAX_SPEED                    8
+// #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              90
+
+// This was really fast after too short a time.
+// #define MOUSEKEY_DELAY                              100
+// #define MOUSEKEY_INTERVAL                           16
+// #define MOUSEKEY_MAX_SPEED                          10
+// #define MOUSEKEY_TIME_TO_MAX                        100
+// #define MOUSEKEY_WHEEL_MAX_SPEED                    8
+// #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              90
